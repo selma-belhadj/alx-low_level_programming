37d2e743a10f2448936e8a433d254253f2c75ac2
Selma <es_belhadj@esi.dz>
2022-09-09 12:02:34 -0700

    all

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low == 'q' || low == 'e')
+{
+continue;
+}
+putchar(low);
+}
+putchar('\n');
+return (0);
+}
