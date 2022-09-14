6c80c2c34c2bb0171c74a9881225e72d19d81e8b
Selma <es_belhadj@esi.dz>
2022-09-14 04:23:25 -0700

    Add description for parameter or member n

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _abs - computer absolute value of integer
- * @int: integer
- * Return: absolute value
+ *  _abs - computer absolute value of integer
+ *  @n: integer
+ *  Description: This function returns absolute value of a number
+ *  Return: Absolute value of number n
  */
 
 int _abs(int n)
