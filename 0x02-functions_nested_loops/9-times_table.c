4283112a3d0e927d2cae037209ec7b25640624cb
Selma <es_belhadj@esi.dz>
2022-09-14 16:17:37 -0700

    Fix betty linter errors

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,34 @@
 
 void times_table(void)
 {
-    int row;
-    int column;
-    int product;
+	int row;
+	int column;
+	int product;
 
-    for (row = 0; row < 10; row++)
-    {
-        for (column = 0; column < 10; column++)
-        {
-            product = row * column;
-            if (column == 0)
-            {
-                _putchar('0' + product);
-            }
-            else if (product <= 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar('0' + product);
-            }
-            else if (product > 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar('0' + (product / 10));
-                _putchar('0' + (product % 10));
-            }
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row < 10; row++)
+	{
+		for (column = 0; column < 10; column++)
+		{
+			product = row * column;
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
