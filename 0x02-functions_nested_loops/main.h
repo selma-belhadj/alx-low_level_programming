2db39abb6d5219abba57c6c8b3d5f81cfba49844
Selma <es_belhadj@esi.dz>
2022-09-14 03:30:00 -0700

    Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,3 +30,11 @@ int _islower(int c);
  * Return: 1 if lower or uppercase, 0 if not alphabet character
  */
 int _isalpha(int c);
+
+/**
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+int print_sign(int n);
+
