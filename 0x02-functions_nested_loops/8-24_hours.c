5985d41f2645cdf996b577b202dc75ef2c823bff
Selma <es_belhadj@esi.dz>
2022-09-14 07:23:10 -0700

    Fix betty linter errors

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,10 @@
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h < 24; h++)
 	{
-		for (m =0; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
