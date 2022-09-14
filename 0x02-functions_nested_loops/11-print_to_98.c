eb443f4e4066345093fc00d51269d31a352eff82
Selma <es_belhadj@esi.dz>
2022-09-14 16:58:42 -0700

    Fix betty linter errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,19 @@
 
 void print_to_98(int n)
 {
-       if (n > 98)
-       {
+	if (n > 98)
+	{
 		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
-       }
-       else if (n < 98)
-       {
-	       for (; n < 98; n++)
-	       {
-		       printf("%d, ", n);
-	       }
-       }
-       printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
