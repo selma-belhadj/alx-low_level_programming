7898f1c1e11f23ff44405cb508b3624633c33999
Selma <es_belhadj@esi.dz>
2022-09-15 00:21:35 -0700

    Fix linter errors + logic" "

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,10 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li %li ", a, b);
+		printf("%li, %li, ", a, b);
 		a += b;
 		b += a;
 	}
-	if (countto % 2 == 1)
-		printf("%li", a);
-
 	printf("\n");
 
 	return (0);
