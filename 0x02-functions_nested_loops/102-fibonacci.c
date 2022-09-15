f31b7dbceeb091dbf35a57e9ef3671eb034c4de6
Selma <es_belhadj@esi.dz>
2022-09-15 00:30:12 -0700

    Fix display output

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,9 +14,16 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li, ", a, b);
-		a += b;
-		b += a;
+		if (counter < 25)
+		{
+			printf("%li, %li, ", a, b);
+			a += b;
+			b += a;
+		}
+		else
+		{
+			printf("%li, %li", a, b);
+		}
 	}
 	printf("\n");
 
