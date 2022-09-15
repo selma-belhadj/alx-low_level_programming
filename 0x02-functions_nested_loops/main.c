dea3cd9b0f222a7eccd351e357ed9470d148258e
Selma <es_belhadj@esi.dz>
2022-09-14 23:58:31 -0700

    Fix betty linter errors

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -14,11 +14,18 @@ int main(void)
 	int n;
 
 	n = add(89, 9);
-	printf("%d\n", n);*/
+	printf("%d\n", n);
 	print_to_98(0);
 	print_to_98(98);
 	print_to_98(111);
 	print_to_98(81);
 	print_to_98(-10);
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);*/
 	return (0);
 }
