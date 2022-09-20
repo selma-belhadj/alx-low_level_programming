09f3f303531cd1138ea050c6ad13327fae2f815b
Selma <es_belhadj@esi.dz>
2022-09-20 13:34:53 -0700

    Add the the end sign of a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,18 +4,18 @@
  * _strcpy - fuction copies the string pointed
  * @dest: pointer to copy the string to.
  * @src: the string to copy to the destination.
- *
  * Return: a copy of the src.
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
-	
+
 	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
 	}
-	return(dest);
+	dest[index] = '\0';
+	return (dest);
 }
