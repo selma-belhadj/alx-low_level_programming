f79522f02655cbb35e768f74346df4afef308fb5
selma <belhadjselma1@gmail.com>
2022-10-03 23:41:49 +0100

    Write a function that splits a string into words.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,7 +22,8 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-/* allocate memory for total number of chars and
+/**
+ * allocate memory for total number of chars and
  * new line for each word
  */
 	strDup = malloc(sizeof(char) * size);
