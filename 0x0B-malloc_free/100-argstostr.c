737bfe3f44ef8e2c961e490c95b7af4bece221d1
selma <belhadjselma1@gmail.com>
2022-10-05 08:19:23 +0100

    Fix betty description error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *argstostr - concatenates all arguements to the program
+ * argstostr - function that concatenates all arguements to the program
  * @ac: arguement count
  * @av: pointer to arguements
  * Return: pointer to new space in memory or null
