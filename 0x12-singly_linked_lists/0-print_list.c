c52c1ebcd2101efd7004c96551a3cac1b973b852
selma <belhadjselma1@gmail.com>
2022-10-20 11:26:34 +0100

    Write a function that prints a sentence before the main function is executed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,25 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
+
 /**
- * print_list - prints all elements of a list_t list
- * @h: singly linked list to print
- * Return: number of nodes in the list
+ * print_list - prints all elements of list_t list
+ * @h: linked list
+ * Return: number of nodes in linked list
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t count = 0;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		count++;
 		h = h->next;
 	}
-	return (i);
+
+	return (count);
 }
