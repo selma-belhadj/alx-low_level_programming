526b9b9bbd6cbe919e43ab340010522ab4a11d1d
selma <belhadjselma1@gmail.com>
2022-12-08 14:41:49 +0100

    Add all the main & needed files

---
+++ 0x17-doubly_linked_lists/2-main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	/* add to empty list */
+	head = NULL;
+	add_dnodeint(&head, 0);
+	print_dlistint(head);
+	printf("____________\n");
+
+	/* add to one node list */
+	head = NULL;
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	print_dlistint(head);
+	printf("____________\n");
+
+	/* add to one node list */
+	head = NULL;
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	add_dnodeint(&head, 4);
+	add_dnodeint(&head, 98);
+	add_dnodeint(&head, 402);
+	add_dnodeint(&head, 1024);
+	print_dlistint(head);
+	printf("____________\n");
+	return (EXIT_SUCCESS);
+}
