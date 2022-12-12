291830e980b4f22e6a099ec319657312472238cb
selma <belhadjselma1@gmail.com>
2022-12-12 21:33:54 +0100

    Update the atoi file by deleting the repeated strlen function

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-
-/**
  * idx_num_starts - find index where a digit is first found in string
  * @s: string to search
  * Return: integer index where digit is first found
