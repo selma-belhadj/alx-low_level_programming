e528551733ecb2425642ef45d4b1ce2990cbd0a5
selma <belhadjselma1@gmail.com>
2022-12-12 22:19:35 +0100

    Fix the conflict with naming files

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,13 +1,5 @@
-#include <unistd.h>
+#!/bin/bash
+gcc -c -fPIC *.c
+gcc *.o -shared -o liball.so
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -c -fPIC *.c
-gcc *.o -shared -o liball.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
